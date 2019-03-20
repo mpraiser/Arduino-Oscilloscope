fb1f1b2761041ca858b610843a6df530d456a741
mpraiser <mraiser@foxmail.com>
2019-03-20 16:31:07 +0800

    add ADC sample rate control and queue buffer.

--- Arduino-Oscilloscope.ino
+++ Arduino-Oscilloscope.ino
@@ -1,8 +1,35 @@
+#define MAX_BUFFER_LENGTH 512
 int sensorPin=A0;
+int PWMPin=3;
+int sample_rate=4000;
+int mybuffer[MAX_BUFFER_LENGTH];
+int buffer_tail=0;
+int buffer_head=0;
 void setup(){
+  pinMode(13,OUTPUT);
     Serial.begin(115200);
+    attachInterrupt(0,RunSample,RISING);//use pin 2 to interrupt
+    tone(PWMPin,sample_rate);
 }
 void loop(){
-    int sensorValue=analogRead(sensorPin);
-    Serial.println(sensorValue);
+  if(buffer_head==buffer_tail){
+    digitalWrite(13,HIGH); //queue is empty, turn on LED to warn  
+  }
+  else{
+    Serial.println(mybuffer[buffer_head]);
+    buffer_head=(buffer_head+1)%MAX_BUFFER_LENGTH;
+  }
+    
+}
+
+void RunSample(){
+   int sensorValue=analogRead(sensorPin);
+   if((buffer_tail+1)%MAX_BUFFER_LENGTH==buffer_head){
+      digitalWrite(13,HIGH); //turn LED on to warn overflow
+   }
+   else{
+      //digitalwrite(13,LOW);
+      mybuffer[buffer_tail]=sensorValue;//push into quene
+      buffer_tail=(buffer_tail+1)%MAX_BUFFER_LENGTH;
+    }
 }
--- monitor.py
+++ monitor.py
@@ -5,14 +5,14 @@ import serial
 
 REF_VOLTAGE = 5
 MAX_ADC_VALUE = 1023
-FRAME_INTERVAL = 400
-MAX_DATA_LENGTH = 800
+FRAME_INTERVAL = 500
+MAX_DATA_LENGTH = 100
 
 myArduino = serial.Serial('COM3', 115200)
 plt.ion()
 data = [ ]
 i = 0
-#start_time = time.time()
+start_time = time.time()
 while (True):
     value = myArduino.readline()  # read the reply
     #print(value)
@@ -33,16 +33,16 @@ while (True):
     #control frame rate to make it in real time
     if i > FRAME_INTERVAL:
         def NewFrame():
-            plt.ylim(0,5)
+            plt.ylim(-0.5,5.5)
             plt.title('Osciloscope')
             plt.grid(True)
             plt.ylabel('ADC outputs')
-            plt.plot(data, 'ro-', label='Channel 0')
+            plt.plot(data, 'ro-', label='Channel 0', marker=',')
             plt.legend(loc='lower right')
         drawnow(NewFrame)  #update plot to reflect new data input
-        #print('Sample Frequency:',FRAME_INTERVAL/(time.time() - start_time))
+        print('Sample Frequency:',FRAME_INTERVAL/(time.time() - start_time))
         i = 0
-        #start_time = time.time()
+        start_time = time.time()
         
     else:
         i += 1
